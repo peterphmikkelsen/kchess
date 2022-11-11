4abd1ebb3ebecc40775ecb54c3387297693d3f1e
Peter Mikkelsen <31919921+peterphmikkelsen@users.noreply.github.com>
2022-11-11 21:15:37 +0100

    introducing background color as global constant

--- src/jvmMain/kotlin/Constants.kt
+++ src/jvmMain/kotlin/Constants.kt
@@ -1,5 +1,8 @@
+import androidx.compose.ui.graphics.Color
+
 class Constants {
     companion object {
         const val SQUARE_SIZE = 120f
+        val BACKGROUND_COLOR = Color(0xfff9f9f9)
     }
 }
