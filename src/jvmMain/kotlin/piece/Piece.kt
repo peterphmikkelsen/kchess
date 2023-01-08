17363067f6b18e155c463aaa7ea48878f31c63f4
Peter Mikkelsen <peterphmikkelsen@gmail.com>
2023-01-08 21:51:12 +0100

    simplify piece position logic
    this unfortunately breaks the attacking of a piece from a UI perspective (the UI does not update but the game logic does)

--- src/jvmMain/kotlin/piece/Piece.kt
+++ src/jvmMain/kotlin/piece/Piece.kt
@@ -3,8 +3,10 @@ package piece
 import Constants
 import Game
 import Position
+import absolutePosition
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,7 +22,7 @@ import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import java.lang.Math.round
+import kotlin.math.roundToInt
 
 
 interface Piece {
@@ -35,39 +37,39 @@ interface Piece {
 @Composable
 fun PieceView(game: Game, piece: Piece?) {
     piece ?: return
-    var prevOffset = Offset(0f, 0f)
-    val offset = mutableStateOf(prevOffset)
+    val squareOffset = Constants.SQUARE_SIZE * 2
+    val offset = mutableStateOf(Offset(piece.position.second * squareOffset, piece.position.first * squareOffset))
 
-    val squareSize = Constants.SQUARE_SIZE
-
-    Box(Modifier.size(squareSize.dp)) {
+    Box(Modifier.size(Constants.SQUARE_SIZE.dp)) {
         Image(
             painter = painterResource("${piece.name}_${piece.color}.svg"),
             contentDescription = "${piece.color} piece.type.${piece.name.capitalize(Locale.current)}",
-            modifier = Modifier.fillMaxSize().zIndex(100f).offset {
-                IntOffset(offset.value.x.toInt(), offset.value.y.toInt())
-            }.onDrag(
-                onDragEnd = {
-                    // Move piece to center of square on release
-                    offset.value = Offset(round(offset.value.x / squareSize) * squareSize, round(offset.value.y / squareSize) * squareSize)
+            modifier = Modifier.fillMaxSize()
+                .offset { IntOffset(offset.value.x.toInt(), offset.value.y.toInt()) }
+                .onDrag(
+                    onDragEnd = {
+                        // Move piece to center of square on release
+                        offset.value = Offset(
+                            (offset.value.x / squareOffset).roundToInt() * squareOffset,
+                            (offset.value.y / squareOffset).roundToInt() * squareOffset
+                        )
 
-                    // Move piece in internal game logic
-                    val newPositionX = (piece.position.first + ((offset.value.y - prevOffset.y) / squareSize)).toInt()
-                    val newPositionY = (piece.position.second + ((offset.value.x - prevOffset.x) / squareSize)).toInt()
-                    val moved = game.board.value.move(piece.position, Position(newPositionX, newPositionY))
+                        // Move piece in internal game logic
+                        val newPositionX = (offset.value.y / squareOffset).toInt()
+                        val newPositionY = (offset.value.x / squareOffset).toInt()
+                        val moved = game.board.value.move(piece.position, Position(newPositionX, newPositionY))
 
-                    // Don't end the turn if the move was not successful
-                    if (!moved) return@onDrag
-                    game.state.endTurn()
+                        // Don't end the turn if the move was not successful
+                        if (!moved) return@onDrag
+                        // TODO: Fix bug that increases offset when this is on
+                        // game.state.endTurn()
 
-                    println(game.board.value)
-
-                    prevOffset = offset.value
+                        println(game.board.value)
+                    }
+                ) {
+                    if (piece.color != game.state.turn.value) return@onDrag
+                    offset.value += it
                 }
-            ) {
-                if (piece.color != game.state.turn.value) return@onDrag
-                offset.value += it
-            }
         )
     }
 }
