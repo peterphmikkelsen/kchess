95a3d08529cbf569611d26a9ef46d89ad315f3e9
Peter Mikkelsen <peterphmikkelsen@gmail.com>
2023-01-19 17:57:54 +0100

    renamed file

--- src/jvmMain/kotlin/utils/Helpers.kt
+++ src/jvmMain/kotlin/utils/Helpers.kt
@@ -1,6 +1,9 @@
 package utils
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 
 class Constants {
     companion object {
@@ -14,3 +17,5 @@ val Color.Companion.DarkSquare: Color
 
 val Color.Companion.LightSquare: Color
     get() = this.LightGray
+
+fun MutableState<Offset>.toInt() = IntOffset(this.value.x.toInt(), this.value.y.toInt())
