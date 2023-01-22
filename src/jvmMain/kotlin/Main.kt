13a57a2c00744dca811fc95cc2d20591033823bf
Peter Mikkelsen <peterphmikkelsen@gmail.com>
2023-01-22 18:37:46 +0100

    game icon added

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,11 +1,22 @@
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
 import game.GameView
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
 fun main() = singleWindowApplication(
-    title = "Kotlin Chess", state = WindowState(size = DpSize(1200.dp, 1000.dp))
+    title = "Kotlin Chess", state = WindowState(size = DpSize(1200.dp, 1000.dp)),
+    icon = getGameIcon().toPainter()
 ) {
     GameView()
+}
+
+private fun getGameIcon(): BufferedImage {
+    return ImageIO.read(File("src/jvmMain/resources/icon.png"))
+        ?: BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)
 }
