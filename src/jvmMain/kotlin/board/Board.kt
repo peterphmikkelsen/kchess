584871cfe35d801cd048e8b628e8f921ad28e673
Peter Mikkelsen <peterphmikkelsen@gmail.com>
2023-01-27 17:25:22 +0100

    move init of board to method

--- src/jvmMain/kotlin/board/Board.kt
+++ src/jvmMain/kotlin/board/Board.kt
@@ -21,30 +21,15 @@ class Board {
     private var board = Array(8) { arrayOfNulls<Piece>(8) }
 
     init {
-        for (i in 0 until 8) {
-            for (j in 0 until 8) {
-                val position = Position(i, j)
-                // Handle pawns
-                if (i == 1) board[i][j] = Pawn(PieceColor.BLACK, position)
-                if (i == 6) board[i][j] = Pawn(PieceColor.WHITE, position)
+        initializeBoard()
+    }
 
-                // Handle other pieces
-                if (i == 0 || i == 7) {
-                    val color = if (i == 0) PieceColor.BLACK else PieceColor.WHITE
-                    board[i][j] = when (j) {
-                        4 -> King(color, position)
-                        3 -> Queen(color, position)
-                        2, 5 -> Bishop(color, position)
-                        1, 6 -> Knight(color, position)
-                        0, 7 -> Rook(color, position)
-                        else -> null
-                    }
-                }
-            }
-        }
+    fun reset() {
+        initializeBoard()
     }
 
     fun move(from: Position, to: Position): Boolean {
+        println(to)
         if (from == to) return false
         if (to.isOutOfBounds()) return false
 
@@ -75,6 +60,30 @@ class Board {
         return pieces
     }
 
+    private fun initializeBoard() {
+        for (i in 0 until 8) {
+            for (j in 0 until 8) {
+                val position = Position(i, j)
+                // Handle pawns
+                if (i == 1) board[i][j] = Pawn(PieceColor.BLACK, position)
+                if (i == 6) board[i][j] = Pawn(PieceColor.WHITE, position)
+
+                // Handle other pieces
+                if (i == 0 || i == 7) {
+                    val color = if (i == 0) PieceColor.BLACK else PieceColor.WHITE
+                    board[i][j] = when (j) {
+                        4 -> King(color, position)
+                        3 -> Queen(color, position)
+                        2, 5 -> Bishop(color, position)
+                        1, 6 -> Knight(color, position)
+                        0, 7 -> Rook(color, position)
+                        else -> null
+                    }
+                }
+            }
+        }
+    }
+
     override fun toString(): String {
         return buildString {
             for (row in board) {
@@ -83,6 +92,10 @@ class Board {
                 this.appendLine()
             }
         }
+    }
+
+    operator fun set(position: Position, value: Piece?) {
+        board[position.first][position.second] = value
     }
 }
 
